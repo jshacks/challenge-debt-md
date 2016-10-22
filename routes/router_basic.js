4e18704c84ce3ebf7fc8a335f86fffff18f3ce5d
Anatolii Bivol <toleabivol@gmail.com>
2016-10-22 17:52:58 +0300

    getting total

--- routes/router_basic.js
+++ routes/router_basic.js
@@ -1,20 +1,21 @@
 const routes = require('express').Router();
-const debtModel = require('../models/debt');
-const creditorModel = require('../models/creditor');
+const Debt = require('../models/debt');
+const Creditor = require('../models/creditor');
 
 routes.get('/debt/total', (req, res) => {
-	var result = model.getTotal();
-	res.json(result)
+	Debt.getTotal(function(total){
+		res.json(total);
+	});
 });
 
 routes.get('/debt/all', (req, res) => {
-	debtModel.getAll(function(err, result){
+	Debt.getAll(function(err, result){
 		res.json(result);
 	});
 });
 
 routes.get('/debt/creditor/:creditor', (req, res) => {	
-	debtModel.getTotalPerCreditor(req.params.creditor,function(result){
+	Debt.getTotalPerCreditor(req.params.creditor,function(err,result){
 		res.json(result);
 	});
 });
